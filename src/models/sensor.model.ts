4f3a12676015242d8daa023e5d038187e4d66352
Fox <kostya.moroz.96@gmail.com>
2024-06-13 13:41:29 +0300

    init

---
+++ src/models/sensor.model.ts
@@ -0,0 +1,25 @@
+export interface Vector3 {
+    x: number;
+    y: number;
+    z: number;
+}
+
+export interface Sensor {
+    id: string;
+    name: string;
+    position: Vector3;
+    waterSpeed: Vector3;
+    thrustersSpeed: Vector3;
+    temperature: number;
+    lost: boolean;
+}
+
+export interface SensorData {
+    id: string;
+    name: string;
+    position: Vector3;
+    waterSpeed: Vector3;
+    thrustersSpeed: Vector3;
+    temperature: number;
+    lost: boolean;
+}
